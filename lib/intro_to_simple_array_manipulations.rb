b22b2e7a0fae9fd4c63180aaa68be4784e655889
Aynalem Lenjiso <aynalem.lenjiso@gmial.com>
2016-07-13 14:19:32 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,49 @@
+
+
+def using_push(array, x)
+array.push(x)
+
+
+end
+
+def using_unshift(array, x)
+   array.unshift(x)
+
+end
+
+def using_pop(array)
+  return array.pop()
+end
+
+def pop_with_args(array)
+
+ return array.pop(2)
+
+end
+def using_shift(array)
+
+  return array.shift()
+end
+  def shift_with_args(array)
+    array.shift(2)
+end
+def using_concat(x, y)
+ return  x.concat y
+end
+def using_insert(array, y)
+  array.insert(4, y)
+end
+
+def using_uniq(array)
+  array.uniq()
+end
+
+def using_flatten(array)
+  array.flatten()
+end
+def using_delete(array, x)
+  array.delete(x)
+end
+def using_delete_at(array, x)
+  array.delete_at(x)
+end
